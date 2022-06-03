c18339edf191f803f5af50add8b185fddccac890
VictorAkan <vikurekong123@gmail.com>
2022-06-03 14:13:14 +0100

    practicing codeAcademy

---
+++ Python/index.py
@@ -0,0 +1,9 @@
+# three different ways to import module
+import module
+module.function()
+# or
+from module import function
+function()
+# or
+from module import *
+function() # this method not supported because of some clusters
